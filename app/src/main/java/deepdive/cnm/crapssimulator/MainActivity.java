9e048d151e141e8b042754d581ce9558595a9df9
finn <isaaclindland@protonmail.com>
2019-09-23 16:44:51 -0600

    added listview and tally line with dummy data

--- app/src/main/java/deepdive/cnm/crapssimulator/MainActivity.java
+++ app/src/main/java/deepdive/cnm/crapssimulator/MainActivity.java
@@ -1,37 +1,49 @@
 package deepdive.cnm.crapssimulator;
 
-import android.view.LayoutInflater.Factory;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    TextView tally = findViewById(R.id.tally);
+    tally.setText("This will be my win/loss tally line");
+    ListView rolls = findViewById(R.id.rolls);
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.single_roll,
+        getResources().getStringArray(R.array.dummy_rolls));
+    rolls.setAdapter(adapter);
+
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
 
-    getMenuInflater().inflate(R.menu.options,menu);
+    getMenuInflater().inflate(R.menu.options, menu);
     return true;
   }
 
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     boolean handled = true;
-    switch (item.getItemId()){
+    switch (item.getItemId()) {
       case R.id.run:
-        Toast.makeText(this, R.string.run_toast,Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.run_toast, Toast.LENGTH_LONG).show();
         break;
       case R.id.reset:
-        Toast.makeText(this, R.string.reset_toast,Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.reset_toast, Toast.LENGTH_LONG).show();
         break;
       default:
         handled = super.onOptionsItemSelected(item);

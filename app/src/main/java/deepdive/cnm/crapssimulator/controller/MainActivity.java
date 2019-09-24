61cb0634e0b6063afa271d9dbf13666625b76f85
finn <isaaclindland@protonmail.com>
2019-09-24 16:58:14 -0600

    added round adapter, added view pkg

--- app/src/main/java/deepdive/cnm/crapssimulator/controller/MainActivity.java
+++ app/src/main/java/deepdive/cnm/crapssimulator/controller/MainActivity.java
@@ -1,4 +1,5 @@
-package deepdive.cnm.crapssimulator;
+package deepdive.cnm.crapssimulator.controller;
+
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,22 +10,33 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.ArrayList;
+import deepdive.cnm.crapssimulator.R;
+import deepdive.cnm.crapssimulator.view.RoundAdapter;
+import edu.cnm.deepdive.craps.model.Game;
+import edu.cnm.deepdive.craps.model.Game.Round;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
+  private Game game;
+  private Random rng;
+  private RoundAdapter adapter;
+  private TextView tally;
+  private ListView rolls;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    TextView tally = findViewById(R.id.tally);
-    tally.setText("This will be my win/loss tally line");
-    ListView rolls = findViewById(R.id.rolls);
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.single_roll,
-        getResources().getStringArray(R.array.dummy_rolls));
+    tally = findViewById(R.id.tally);
+    rolls = findViewById(R.id.rolls);
+    adapter = new RoundAdapter(this);
     rolls.setAdapter(adapter);
+    rng = new Random();
+    resetGame();
+
 
   }
 
@@ -40,15 +52,28 @@ public class MainActivity extends AppCompatActivity {
     boolean handled = true;
     switch (item.getItemId()) {
       case R.id.run:
-        Toast.makeText(this, R.string.run_toast, Toast.LENGTH_LONG).show();
+        updateDisplay(game.play());
         break;
       case R.id.reset:
-        Toast.makeText(this, R.string.reset_toast, Toast.LENGTH_LONG).show();
+        resetGame();
         break;
       default:
         handled = super.onOptionsItemSelected(item);
+
     }
 
     return handled;
+  }
+
+  private void updateDisplay(Round round) {
+
+    adapter.add(round);
+    tally.setText(getString(R.string.tally_format, game.getWins(), game.getPlays(),
+        100 * game.getPercentage()));
+  }
+
+  private void resetGame(){
+    game = new Game(rng);
+    updateDisplay(null);
   }
 }

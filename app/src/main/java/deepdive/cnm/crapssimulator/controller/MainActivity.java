79f269ff82445b175ebb280102beb6722760cb2d
finn <isaaclindland@protonmail.com>
2019-09-25 15:49:40 -0600

    implemented using a runnable

--- app/src/main/java/deepdive/cnm/crapssimulator/controller/MainActivity.java
+++ app/src/main/java/deepdive/cnm/crapssimulator/controller/MainActivity.java
@@ -3,10 +3,8 @@ package deepdive.cnm.crapssimulator.controller;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,10 +13,15 @@ import deepdive.cnm.crapssimulator.view.RoundAdapter;
 import edu.cnm.deepdive.craps.model.Game;
 import edu.cnm.deepdive.craps.model.Game.Round;
 import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class MainActivity extends AppCompatActivity {
 
+  private static ExecutorService executor = Executors.newSingleThreadExecutor();
+  private Boolean running = false;
   private Game game;
   private Random rng;
   private RoundAdapter adapter;
@@ -48,14 +51,33 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    super.onPrepareOptionsMenu(menu);
+    menu.findItem(R.id.play_one).setVisible(!running);
+    menu.findItem(R.id.fast_forward).setVisible(!running);
+    menu.findItem(R.id.pause).setVisible(running);
+    menu.findItem(R.id.reset).setEnabled(!running);
+    return true;
+
+  }
+
+  @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     boolean handled = true;
     switch (item.getItemId()) {
-      case R.id.run:
-        updateDisplay(game.play());
+      case R.id.play_one:
+        updateDisplay(game.play(),game.getWins(),game.getLosses(),game.getPercentage());
         break;
       case R.id.reset:
         resetGame();
+        break;
+      case R.id.fast_forward:
+        executor.execute(new Runner());
+        invalidateOptionsMenu();
+        running = true;
+        break;
+      case R.id.pause:
+        running = false;
         break;
       default:
         handled = super.onOptionsItemSelected(item);
@@ -65,15 +87,69 @@ public class MainActivity extends AppCompatActivity {
     return handled;
   }
 
-  private void updateDisplay(Round round) {
-
+  private void updateDisplay(Round round, int wins, int plays, double percentage) {
     adapter.add(round);
-    tally.setText(getString(R.string.tally_format, game.getWins(), game.getPlays(),
-        100 * game.getPercentage()));
+    tally.setText(getString(R.string.tally_format, wins, plays, 100 * percentage));
   }
 
   private void resetGame(){
     game = new Game(rng);
-    updateDisplay(null);
+    updateDisplay(null,0,0,0);
   }
+
+  private class Runner implements Runnable{
+
+    private int plays;
+    private int wins;
+    private double percentage;
+    private Round round;
+
+    @Override
+    public void run() {
+
+      while (running){
+        round = game.play();
+
+        if(game.getPlays() % 500000 == 0){
+          wins = game.getWins();
+          plays = game.getPlays();
+          percentage = game.getPercentage();
+          runOnUiThread(()->{
+            updateDisplay(round, wins, plays, percentage);
+          });
+        }
+
+      }
+      wins = game.getWins();
+      plays = game.getPlays();
+      percentage = game.getPercentage();
+      runOnUiThread(()->{
+        updateDisplay(round, wins, plays, percentage);
+      });
+
+      invalidateOptionsMenu();
+    }
+  }
+
+  private class Updater implements Runnable{
+
+    private final Round round;
+    private final int wins;
+    private final int plays;
+    private final double percentage;
+
+    public Updater(Round round, int wins, int plays, double percentage) {
+      this.round = round;
+      this.wins = wins;
+      this.plays = plays;
+      this.percentage = percentage;
+    }
+
+    @Override
+    public void run() {
+
+
+    }
+  }
+
 }
